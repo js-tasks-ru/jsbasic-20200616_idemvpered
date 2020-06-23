d95144e13b9c38afeb9fd435dc36620b379491ba
Vladimir Popov <idemvpered@yandex.ru>
2020-06-23 19:27:14 +0600

    нашел решение, все понял, разобрался

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -3,6 +3,12 @@
  * @param {number} n
  * @returns {number}
  */
-function factorial(n) {
-  // ваш код...
+function factorial(n)
+{
+  let result = 1;
+  while(n)
+  {
+      result *= n--;
+  }
+  return result;
 }

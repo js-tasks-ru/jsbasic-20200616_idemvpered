73eb6becb5b8202839bec51aa181851e131336b4
Vladimir Popov <idemvpered@yandex.ru>
2020-06-25 10:59:48 +0600

    на основании текста учебника сделал задачку

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -3,6 +3,10 @@
  * @param {string} str base
  * @returns {boolean}
  */
-function checkSpam(str) {
-  // ваш код...
-}
+function checkSpam(str)
+  {
+   let lowerStr = str.toLowerCase();
+  return lowerStr.includes('1xbet') || lowerStr.includes('xxx')
+  
+  }
+  

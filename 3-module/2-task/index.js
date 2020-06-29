b2d1985d16195a48ea9d5b8c95899c52aba16d8a
Vladimir Popov <idemvpered@yandex.ru>
2020-06-29 21:56:05 +0600

    разобрал с другом

--- 3-module/2-task/index.js
+++ 3-module/2-task/index.js
@@ -1,9 +1,25 @@
+'use strict'
 /**
  * @param {number[]} arr
  * @param {number} a
  * @param {number} b
  * @returns {number[]}
  */
+
 function filterRange(arr, a, b) {
-  // ваш код...
+  return arr.filter(value => {
+    if (a < b && value >= a && value <= b) { 
+      return value
+    }
+    if (a > b && value <= a && value >= b) { 
+      return value
+    }
+    return false
+  });
 }
+let arr = [5, 3, 8, 1];
+
+let filtered = filterRange(arr, 4, 4);
+
+console.log( filtered ); // [3,1] (совпадающие значения)
+console.log( arr ); // [5,3,8,1] (без изменений)

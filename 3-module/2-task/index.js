2846bf2ece4a0d55959fa9517b368cedc2d60752
Vladimir Popov <idemvpered@yandex.ru>
2020-06-29 22:11:50 +0600

    описание того, что вы сделали

--- 3-module/2-task/index.js
+++ 3-module/2-task/index.js
@@ -1,4 +1,4 @@
-'use strict'
+
 /**
  * @param {number[]} arr
  * @param {number} a
@@ -19,7 +19,4 @@ function filterRange(arr, a, b) {
 }
 let arr = [5, 3, 8, 1];
 
-let filtered = filterRange(arr, 4, 4);
 
-console.log( filtered ); // [3,1] (совпадающие значения)
-console.log( arr ); // [5,3,8,1] (без изменений)

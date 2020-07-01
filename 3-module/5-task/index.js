8c56dd035a7388ed0b7b5689ec572edc3a282271
Vladimir Popov <idemvpered@yandex.ru>
2020-07-01 21:52:53 +0600

    с другом решил через регулярку

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -3,6 +3,14 @@
  * @param   {string} str -  входные данные
  * @returns {{min:number, max:number}}  объект
  */
+
+let inputData = '25,-1,-234,4,   1000';
+
 function getMinMax(str) {
-  // ваш код...
+  var prices    = str.match(/[+-]?([0-9]*[.])?[0-9]+/g).sort(function(a,b) {
+      return a - b
+    })
+    return {min:parseFloat(prices[0]), max:parseFloat(prices[prices.length - 1 ])}
 }
+
+//console.log(getMinMax(inputData)); // { min: -5.8, max: 73  }
